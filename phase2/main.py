2a5d9ed6684c7367623e34c691d891a4b7a6d2da
Amirhosein Rostami <amirhssin6rst@gmail.com>
2022-05-05 21:36:58 +0430

    initialize Phase 2

---
+++ phase2/main.py
@@ -0,0 +1,28 @@
+import sys, getopt
+import tokenizer
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print ('main.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+    with open("tests/" + inputfile, "r") as input_file:
+        output = tokenizer.tokenize(inputfile,input_file.readlines())
+
+    with open("out/" + outputfile, "w") as output_file:
+        output_file.write(output)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
